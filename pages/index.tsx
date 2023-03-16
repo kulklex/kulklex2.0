6afb63ae68da6faef21af01fe949f6ed861ea638
kulklex <kulklex@gmail.com>
2023-03-17 00:45:06 +0100

    index page setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
+import Features from '../components/Features'
+import Projects from '../components/Projects'
+import Resume from '../components/Resume'
+import Testimonial from '../components/Testimonial'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+import FooterBottom from '../components/FooterBottom'
 
 const Home: NextPage = () => {
   return (
-    <div>Home</div>
+    <div className="w-full h-auto app__bg text-lightText px-4">
+      <Navbar />
+      <div className="max-w-screen-xl mx-auto py-4">
+        <Banner />
+        <Features />
+        <Projects />
+        <Resume />
+        <Testimonial />
+        <Contact />
+        <Footer />
+        <FooterBottom />
+      </div>
+    </div>
   )
 }
 
