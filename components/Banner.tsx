4348ad3313cee6b6f36fe75bf399658a108b6655
kulklex <kulklex@gmail.com>
2023-03-17 09:43:42 +0100

    Icons Setup

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import {useTypewriter, Cursor} from 'react-simple-typewriter'
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa"
-import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si"
+import {  BsGit } from 'react-icons/bs'
+import {  AiOutlineHtml5 } from 'react-icons/ai'
+import {  TbBrandReactNative } from 'react-icons/tb'
+import { DiNodejs, DiPython, DiDjango, DiMongodb, DiJava } from 'react-icons/di'
+import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa"
+import { SiTailwindcss, SiMysql, SiPostgresql, SiBootstrap, SiSpringboot, SiSpringsecurity, SiFigma, SiNextdotjs, SiTypescript, SiRedux } from "react-icons/si"
 import banner from '../public/images/bannerImg.png'
 
 type Props = {}
@@ -17,11 +22,11 @@ export default function Banner({}: Props) {
   })
 
   return (
-    <section id="home" className='w-full h-[800px] pb-20 flex flex-col md:flex-row border-b-[1px] border-b-designColor font-titleFont'>
+    <section id="home" className='w-full h-full pb-20 flex flex-col md:flex-row border-b-[1px] border-b-designColor font-titleFont'>
         
         {/* LEFT SIDE START*/}
         <div className="w-full md:w-1/2">
-          <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-5  flex-[1]">
             <h4 className="text-lg font-normal">WELCOME TO MY PORTFOLIO</h4>
             <h1 className="text-3xl md:text-5xl font-bold text-white">
               Hi, I’m {" "}<span className="capitalize text-designColor"> Hassan Yusuff</span>
@@ -37,40 +42,85 @@ export default function Banner({}: Props) {
           </div>
 
 
-          <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between py-10">
+          <div className="flex flex-col flex-[1] gap-6  justify-between py-10">
             <div>
               <h2 className="text-base text-designColor uppercase font-titleFont mb-4">
-                Find me in
+                TECHNOLOGIES
               </h2>
-              <div className="flex gap-4">
-                <span className="bannerIcon">
-                  <FaFacebookF />
+              <div className="flex flex-wrap gap-4 items-center justify-center">
+                <span className="bannerIcon text-extrabold text-4xl text-blue-300">
+                  <FaReact />
                 </span>
-                <span className="bannerIcon">
-                  <FaTwitter />
+                <span className="bannerIcon text-extrabold text-4xl text-green-300">
+                  <DiNodejs />
                 </span>
-                <span className="bannerIcon">
-                  <FaLinkedinIn />
+                <span className="bannerIcon text-extrabold text-4xl text-green-800">
+                  <DiMongodb />
+                </span>
+                <span className="bannerIcon text-extrabold text-4xl">
+                  <SiNextdotjs />
+                </span>
+                <span className="bannerIcon text-extrabold text-4xl text-blue-600">
+                  <SiTypescript />
+                </span>
+                <span className="bannerIcon text-extrabold text-4xl text-purple-500">
+                  <SiRedux />
+                </span>
+                <span className="bannerIcon text-extrabold text-4xl text-blue-400">
+                  <SiTailwindcss />
+                </span>
+                <span className="bannerIcon text-extrabold text-4xl text-purple-700">
+                  <SiBootstrap />
+                </span>
+                <span className="bannerIcon text-extrabold text-4xl text-orange-800">
+                  <BsGit />
+                </span>
+                <span className="bannerIcon  text-extrabold text-4xl text-orange-700">
+                  <DiJava />
+                </span>
+                <span className="bannerIcon text-extrabold text-4xl text-green-600">
+                  <SiSpringboot />
+                </span>
+                <span className="bannerIcon text-extrabold text-4xl text-green-800">
+                  <SiSpringsecurity />
+                </span>
+                <span className="bannerIcon text-extrabold text-4xl text-white">
+                  <DiPython />
+                </span>
+                <span className="bannerIcon  text-extrabold text-4xl text-white">
+                  <DiDjango />
+                </span>
+                <span className="bannerIcon  text-extrabold text-4xl text-blue-200">
+                  <SiMysql />
+                </span>
+                <span className="bannerIcon  text-extrabold text-4xl text-blue-800">
+                  <SiPostgresql />
+                </span>
+                <span className="bannerIcon text-extrabold text-4xl text-blue-800">
+                  <TbBrandReactNative />
+                </span>
+                <span className="bannerIcon text-extrabold text-4xl text-orange-500">
+                  <AiOutlineHtml5 />
                 </span>
               </div>
             </div>
 
-            <div>
+            <div className="mt-10">
               <h2 className="text-base text-designColor uppercase font-titleFont mb-4">
-                BEST SKILL ON
+                Find me in
               </h2>
               <div className="flex gap-4">
-                <span className="bannerIcon">
-                  <FaReact />
+                <span className="bannerIcon bg-blue-400 text-4xl">
+                  <Link href="/" ><FaFacebookF /></Link>
                 </span>
-                <span className="bannerIcon">
-                  <SiNextdotjs />
+                <span className="bannerIcon bg-blue-800 text-4xl">
+                  <Link href="/" ><FaTwitter /></Link>
                 </span>
-                <span className="bannerIcon">
-                  <SiTailwindcss />
+                <span className="bannerIcon bg-blue-600 text-3xl">
+                  <Link href="/" ><FaLinkedinIn /></Link>
                 </span>
-                <span className="bannerIcon">
-                  <SiFigma />
+                <span className="bannerIcon instagramIcon text-4xl">
+                  <Link href="/" ><FaInstagram /></Link>
                 </span>
               </div>
             </div>
@@ -80,7 +130,7 @@ export default function Banner({}: Props) {
 
 
         {/* RIGHT SIDE START*/}
-        <div className="w-full md:w-1/2 flex justify-center items-center relative">
+        <div className="w-full md:w-1/2 h-full flex justify-center items-center relative">
           <Image src={banner} alt="banner" className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10" />
           <div  className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"  />
         </div> 
