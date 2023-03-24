05a89ba9258358329e0d0fafa47401b5cb2b66e2
kulklex <kulklex@gmail.com>
2023-03-24 09:48:55 +0100

    Images Import impl

--- public/index.ts
+++ public/index.ts
@@ -1,6 +1,21 @@
 import logo from "./images/profile-picture.jpg";
 import contactImg from "./images/contact/contactImg.png";
 import profilePic from "./images/profile.jpeg"
+import calcRoute from "./images/projects/calculate-route.png"
+import cryptoAPI from "./images/projects/cyptoAPI.png"
+import graphCMSBlog from './images/projects/graphcmsBlog.png'
+import modernUI from "./images/projects/modernUI.png"
+import pgtwUI from "./images/projects/pgtwUI.png"
+import pixabayAPI from "./images/projects/pixabayAPI.png"
+import realEstateAPI from "./images/projects/real-estate-API.png"
+import restaurantUI from "./images/projects/restaurantUI.png"
+import videoApp from "./images/projects/social-video-app.png"
+import travelAdvisor from "./images/projects/travel-advisory.png"
 
 
-export {logo,  contactImg,  profilePic,}
+
+
+
+
+
+export {logo, restaurantUI, videoApp, travelAdvisor, modernUI, pgtwUI, pixabayAPI, realEstateAPI, calcRoute, graphCMSBlog, cryptoAPI,   contactImg,  profilePic,}
