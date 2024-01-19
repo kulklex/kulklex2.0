2998bcafd277192347c854a030e0ba5f998efd36
Hassan Yusuff <kulklex@gmail.com>
2024-01-19 22:23:11 +0000

    Update Projects.tsx

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -57,12 +57,6 @@ export default function Projects({}: Props) {
             <Title desc="My Projects" />
         </div>
 
-        <div className="text-xs flex justify-center item-center text-center pb-4 text-white hover:text-designColor"> 
-          <p>Click here to access more projects on my GitHub page {"  "}</p>
-          <span className="bg-black text-white flex items-center justify-center text-center px-2">
-            <Link href="https://github.com/kulklex/" target='_blank'><FaGithub /></Link>
-          </span>
-        </div>
 
       <div className="flex justify-center item-center text-center py-4 text-white hover:text-designColor"> 
         Navigate to the next project by using the arrow.
@@ -103,6 +97,13 @@ export default function Projects({}: Props) {
             <ProjectsCard title="Modern UI Landing-Page" desc="A complete responsive modern website design only" url={`https://nextjs-modern-ui-responsive-landing-page.vercel.app/`} gitHub="https://github.com/kulklex/ai-responsive-landing-page-ui" img={modernUI} />
           </div> */}
         </Slider>
+
+         <div className="text-xs flex justify-center item-center text-center pb-4 text-white hover:text-designColor"> 
+          <p>Click here to access more projects on my GitHub page {"  "}</p>
+          <span className="bg-black text-white flex items-center justify-center text-center px-2">
+            <Link href="https://github.com/kulklex/" target='_blank'><FaGithub /></Link>
+          </span>
+        </div>
     </section>
   )
 }
