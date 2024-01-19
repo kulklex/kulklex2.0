4f126c659c16be2f55df5b4c5687dbb183ebec70
Hassan Yusuff <kulklex@gmail.com>
2024-01-19 19:47:00 +0000

    Update Projects.tsx

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -69,10 +69,10 @@ export default function Projects({}: Props) {
             <ProjectsCard title="React Native Jobs API" desc="A cross-platform mobile app that shows available jobs using an external API" url={``} gitHub="https://github.com/kulklex/React-Native-Jobs-App" img={nativeJobsAPI} />
           </div>
           <div>
-            <ProjectsCard title="Pixabay API" desc="Displaying Dynamic features with Pixabay API" url={`https://pictures-app-with-pixabayapi-git-pixabay-api-kulklex.vercel.app/`} gitHub="https://github.com/kulklex/React-Pixabay-Api" img={pixabayAPI} />
+            <ProjectsCard title="Blog using GraphCMS" desc="A Nextjs complete blog app retrieving data with GraphCMS" url={`https://next-js-blog-with-graph-ql-iz78.vercel.app/`} gitHub="https://github.com/kulklex/NextJsBlog-With-GraphQL" img={graphCMSBlog} />
           </div>
           <div>
-            <ProjectsCard title="Live Crypto Coin Ranking APP" desc="A Cryptocurrency web app that fetches and displays different crypto data from an external API" url={`https://crypto-coin-ranking-app.vercel.app/`} gitHub="https://github.com/kulklex/Crypto-Coin-Ranking-App" img={cryptoAPI} />
+            <ProjectsCard title="Pixabay API" desc="Displaying Dynamic features with Pixabay API" url={`https://pictures-app-with-pixabayapi-git-pixabay-api-kulklex.vercel.app/`} gitHub="https://github.com/kulklex/React-Pixabay-Api" img={pixabayAPI} />
           </div>
           <div>
             <ProjectsCard title="TRAVEL-ADVISOR-APP" desc="A Travel advisor app that searches for Restaurants, Hotels and Attractions" url={`https://travel-advisor-app-sable.vercel.app/`} gitHub="https://github.com/kulklex/Travel-Advisor-App" img={travelAdvisor} />
@@ -81,15 +81,15 @@ export default function Projects({}: Props) {
             <ProjectsCard title="Social-Video-App" desc="A Social Media App that allows you log in with Google and upload videos, you can also watch other accounts videos. Built with Nextjs." url={`https://social-media-video-app.vercel.app/`} gitHub="https://github.com/kulklex/social-media-video-app" img={videoApp} />
           </div>
           <div>
-            <ProjectsCard title="Real Estate Api" desc="A Nextjs Real-Estate App retrieving data from an external API with search-by-filters functionality" url={`https://nextjs-real-estate-app-tau.vercel.app/`} gitHub="https://github.com/kulklex/Nextjs-Real-Estate-App" img={realEstateAPI} />
+            <ProjectsCard title="Live Crypto Coin Ranking APP" desc="A Cryptocurrency web app that fetches and displays different crypto data from an external API" url={`https://crypto-coin-ranking-app.vercel.app/`} gitHub="https://github.com/kulklex/Crypto-Coin-Ranking-App" img={cryptoAPI} />
           </div>
           <div>
-            <ProjectsCard title="Blog using GraphCMS" desc="A Nextjs complete blog app retrieving data with GraphCMS" url={`https://next-js-blog-with-graph-ql-iz78.vercel.app/`} gitHub="https://github.com/kulklex/NextJsBlog-With-GraphQL" img={graphCMSBlog} />
+            <ProjectsCard title="Real Estate Api" desc="A Nextjs Real-Estate App retrieving data from an external API with search-by-filters functionality" url={`https://nextjs-real-estate-app-tau.vercel.app/`} gitHub="https://github.com/kulklex/Nextjs-Real-Estate-App" img={realEstateAPI} />
           </div>
           <div>
             <ProjectsCard title="Calculate Route App" desc="A React App that calculates routes using Google maps API" url={`https://google-maps-api-react.vercel.app/`} gitHub="https://github.com/kulklex/google-maps-api-react" img={calcRoute} />
           </div>
-          <div>
+{/*           <div>
             <ProjectsCard title="Payment Gateway UI" desc="A complete responsive payment-gateway-lookalike website design only" url={`https://payment-gtw-landingpage-ui.vercel.app/`} gitHub="https://github.com/kulklex/payment-gtw-landingpage-ui" img={pgtwUI} />
           </div>
           <div>
@@ -97,7 +97,7 @@ export default function Projects({}: Props) {
           </div>
           <div>
             <ProjectsCard title="Modern UI Landing-Page" desc="A complete responsive modern website design only" url={`https://nextjs-modern-ui-responsive-landing-page.vercel.app/`} gitHub="https://github.com/kulklex/ai-responsive-landing-page-ui" img={modernUI} />
-          </div>
+          </div> */}
         </Slider>
     </section>
   )
