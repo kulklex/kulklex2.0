68ba0fc2f0d52abaa19d4988e93eb1dc75a7f53f
Hassan Yusuff <kulklex@gmail.com>
2024-05-14 00:41:47 +0100

    Update Education.tsx

--- components/Resume/Education.tsx
+++ components/Resume/Education.tsx
@@ -9,7 +9,6 @@ export default function Education({}: Props) {
   return (
     <motion.div className="w-full px-4 font-titleFont gap-10 lgl:gap-20"  initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 0.5 } }}>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">1998 - 2010</p>
           <h2 className="text-3xl md:text-4xl font-bold text-white">Education</h2>
         </div>
 
